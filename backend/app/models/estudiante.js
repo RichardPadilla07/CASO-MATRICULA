0b148bdbd754c44b82da14a89f55bc03245964d6
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-08-27 11:57:35 -0500

    3

--- backend/app/models/estudiante.js
+++ backend/app/models/estudiante.js
@@ -13,7 +13,7 @@ const estudianteSchema = new mongoose.Schema({
   direccion: { type: String, maxlength: 20 },
   telefono: { type: String, maxlength: 10 },
   fecha_nacimiento: { type: Date },
-  passwordEstudiante: { type: String }
+  passwordEstudiante: { type: String, maxlength: 100 }
 });
 
 const Estudiante = mongoose.model("Estudiante", estudianteSchema);
